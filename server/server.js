8a94b580542d85da70cbcc99f248c60fa7848122
simon <kedjek177@gmail.com>
2024-04-11 15:32:05 -0400

    mongodb edits

--- server/server.js
+++ server/server.js
@@ -6,29 +6,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 const apiRouter = require('./routes/api');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://linsimon95:rOB1w3L4mrJueXt8@cluster0.hxoymdn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+const mongoose = require('mongoose');
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 // Handle parsing request body
 app.use(express.json());
