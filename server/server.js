49b349bc7baf2d993d94ff3b644987f71b276456
simon <kedjek177@gmail.com>
2024-04-11 14:49:31 -0400

    altered mongodb connection string for vercel

--- server/server.js
+++ server/server.js
@@ -1,21 +1,34 @@
 const path = require ('path');
 const express = require ('express');
-const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const apiRouter = require('./routes/api');
 
-
-const mongoURI = 'mongodb://localhost/ZealthyExercise';
-mongoose.connect(mongoURI, {})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://linsimon95:rOB1w3L4mrJueXt8@cluster0.hxoymdn.mongodb.net/";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Handle parsing request body
 app.use(express.json());
