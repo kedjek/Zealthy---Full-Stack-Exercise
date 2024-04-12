2a6b65dc250b8aa7ee18ba3602b9c68918fd828a
simon <kedjek177@gmail.com>
2024-04-11 20:10:20 -0400

    testing url

--- client/components/ticket_form.jsx
+++ client/components/ticket_form.jsx
@@ -25,15 +25,13 @@ const TicketForm = (props) => {
             console.log('Would normally send email here with body: ', formData)
             console.log( "vercel url is written here")
 
-            // const response = await fetch(`/ticketformsent`, {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            //     body: JSON.stringify(formData),
-            // });
-            const response = await fetch(`/`, {
-          });
+            const response = await fetch(`/ticketformsent`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
             if (response.ok) {
               console.log('Email sent successfully!');
               // Refresh the page after successful submission
